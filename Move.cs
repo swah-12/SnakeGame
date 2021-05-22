5249958f905ca0b25e0c14463c0e34dd58307e07
swah-12 <81489595+swah-12@users.noreply.github.com>
2021-05-22 15:31:14 -0600

    Add files via upload

---
+++ Move.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Snake
+{
+    public class Move
+    {
+        public Move()
+        {
+
+            //get the index of snake
+            //know all obstacles
+
+            //while not hit obstacles or move outside the area, 
+            //four differnts method
+            //if user press the up arrow, move y + px
+            //if user press the down arrow, move y - px
+            //if user press the up left, move x - px
+            //if user press the up right, move x + px
+
+            //keep track of current x and y
+            //sleep
+
+            //hit obstacles,
+            //no- sleep
+
+            //endgame
+            //sound effect
+            //accounter to keep track of the second snake move
+            //print out the counter/result
+
+            //----------------------------------------------
+            //console.keypress
+
+            //if keypress hit right arrow
+                //while don't hit obstacles,
+                    //them move x + px
+                    //keep track of x and y
+                        //if not hit obstacles
+                            //sleep
+                            //keep track of time
+                        //else
+                            //end game
+                            //sound effect
+                            //display result
+        }
+
+
+    }
+}
